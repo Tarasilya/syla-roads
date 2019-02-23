7067ad47ff125955c6fd870db8ad7390900f7af5
Taras Zhylenko <zhylenko@mit.edu>
2019-02-23 10:14:18 -0500

    sending packet v.0

--- client/main.cpp
+++ client/main.cpp
@@ -6,7 +6,15 @@ char* SERVER_NAME;
 int SERVER_PORT;
 
 int main() {
-	return 1;
+	// return 1;
 	sf::TcpSocket socket;
+	socket.setBlocking(false);
 	sf::Socket::Status status = socket.connect(SERVER_NAME, SERVER_PORT);
+
+	sf::Uint16 x = 27;
+	std::string s = "Citizen";
+	sf::Packet packet;
+	packet << x << s;
+	socket.send(packet);
+
 }
