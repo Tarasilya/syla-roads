acec9381302b1ba8f04243acb402821f65c8945e
Taras Zhylenko <zhylenko@mit.edu>
2019-02-23 10:10:46 -0500

    Linked libraries successfully

--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <SFML/Network.hpp>
 
 using namespace std;
+char* SERVER_NAME;
+int SERVER_PORT;
 
 int main() {
-    cout << 1;
+	return 1;
+	sf::TcpSocket socket;
+	sf::Socket::Status status = socket.connect(SERVER_NAME, SERVER_PORT);
 }
