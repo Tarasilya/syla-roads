ac24463c7b08748f72f4fa28aedd09eb057412ce
Illya Shevchenko <ilya.shevchen@gmail.com>
2019-02-24 17:53:28 +0300

    server done

--- server/main.cpp
+++ server/main.cpp
@@ -1,7 +1,36 @@
+#include <SFML/Network.hpp>
+
+#include <string>
 #include <iostream>
+
+const int PORT = 44444;
+
 
 using namespace std;
 
 int main() {
-    cout << 1;
+    sf::TcpListener listener;
+
+    cerr << "trying to listen" << std::endl;
+    if (listener.listen(PORT) != sf::Socket::Done)
+    {
+        // error...
+    }
+    cerr << "listening to " << PORT << std::endl;
+
+    cerr << "trying to accept..." << std::endl;
+    sf::TcpSocket socket;
+    if (listener.accept(socket) != sf::Socket::Done)
+    {
+        // error...
+    }
+    cerr << "connection accepted" << std::endl;
+
+    while(true) {
+        sf::Packet packet;
+        socket.receive(packet);
+        std::string x;
+        packet >> x;
+        std::cout << x << std::endl;
+    }
 }
