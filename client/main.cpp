32ce0cee0614231680bef122e1f516f08cac7a66
Illya Shevchenko <ilya.shevchen@gmail.com>
2019-02-26 07:59:48 +0300

    blown AwAy

--- client/main.cpp
+++ client/main.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <mutex>
 #include <SFML/Network.hpp>
 
 using namespace std;
-char* SERVER_NAME = "127.0.0.1";
+char* SERVER_NAME = "localhost";
 int SERVER_PORT = 44444;
 
 int main() {
 	// return 1;
 	sf::TcpSocket socket;
-	sf::Socket::Status status = socket.connect(SERVER_NAME, SERVER_PORT);
+    socket.setBlocking(false);
+    sf::Socket::Status status = socket.connect(SERVER_NAME, SERVER_PORT);
 
-	std::string s;
-	while(true){
-		std::cin >> s;
-		sf::Packet packet;
-		packet << s;
-		socket.send(packet);
-	}
+    mutex m;
+
+    std::thread t1 ([&]
+    {
+        std::string s;
+        while(true) {
+            getline(cin, s);
+            m.lock();
+            sf::Packet packet;
+            packet << s;
+            socket.send(packet);
+            m.unlock();
+        }
+    });
+    std::thread t2 ([&]
+    {
+        std::string s;
+        while(true) {
+            m.lock();
+            sf::Packet packet;
+            if (socket.receive(packet) == sf::TcpSocket::Status::Done) {
+                packet >> s;
+                std::cout << "(" << s << ")" << std::endl;
+            }
+            m.unlock();
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    });
+    t1.join();
+    t2.join();	
 	
 }
