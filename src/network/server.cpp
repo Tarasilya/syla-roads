7fd1d9825075e5f4a11b31545172e796a4c27b6d
Illya Shevchenko <ilya.shevchen@gmail.com>
2019-02-26 17:42:09 +0300

    coll server

---
+++ src/network/server.cpp
@@ -0,0 +1,48 @@
+#include <SFML/Network.hpp>
+
+#include <string>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <chrono>
+
+const int PORT = 44444;
+const int PLAYERS = 2;
+
+using namespace std;
+
+
+int main() {
+    sf::TcpListener listener;
+
+    cerr << "trying to listen" << std::endl;
+    if (listener.listen(PORT) != sf::Socket::Done)
+    {
+        std::cerr << "ERROR LISTENING TO PORT " << PORT << std::endl;
+    }
+    cerr << "listening to " << PORT << std::endl;
+
+    std::vector<sf::TcpSocket> sockets;
+
+    for (int i = 0; i < PLAYERS; i++) {
+        std::cerr << "trying to accept " << i << std::endl;
+        sf::TcpSocket socket;
+        socket.setBlocking(false);
+        if (listener.accept(socket) != sf::Socket::Done)
+        {
+            cerr << "ERROR ACCEPTING " << i << std::endl;
+            exit(1);
+        }
+
+        std::cerr << "connection " << i << " accepted" << std::endl;
+    }
+    while(true) {
+        for (int i = 0; i < PLAYERS; i++) {
+            sf::Packet packet;
+            if (sockets[i].receive(packet) == sf::TcpSocket::Status::Done) {
+                sockets[1-i].send(packet);
+                std::cerr << "vam posilka" << std::endl;
+            }
+        }
+    }
+}
